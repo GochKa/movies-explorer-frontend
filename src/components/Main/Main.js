1b9d44cb8e2be5ecf2dc6fa0e8e1d7542e1a251e
GochKa <gocha.workout@gmail.com>
2022-07-19 00:09:33 +0300

    Импорт компонентов в Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,18 @@ import React from "react";
 import Header from "../Header/Header";
 import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
+import Techs from "../Techs/Techs"
+import Portfolio from "../Portfolio/Portfolio";
+import Footer from "../Footer/Footer";
 function Main(){
   return(
     <>
       <Header />
       <Promo />
       <AboutProject />
+      <Techs />
+      <Portfolio />
+      <Footer />
     </>
   )
 }
