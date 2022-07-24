0b514265ba153a3d79b90435529a70e33aae0d29
GochKa <gocha.workout@gmail.com>
2022-07-25 02:20:08 +0300

    Адавтив десктоп Promo

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -7,10 +7,15 @@ function Promo(){
     <section className="promo">
       <div className="promo__info">
         <img src={promo__logo} alt="Картнка промо" className="promo__img"></img>
-        <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+        <div className="promo__flex">
+        <h1 className="promo__title">Учебный проект студента факультета
+        <br>
+        </br> Веб-разработки.</h1>
         <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+        <button className="promo__button">Узнать больше</button>     
       </div>
-      <button className="promo__button">Узнать больше</button>
+
+      </div>
     </section>
   )
 }
