ad19f73fe0e2880842cc6de766b8b62af3d0ef11
GochKa <gocha.workout@gmail.com>
2022-07-25 00:52:18 +0300

    Адаптив планшета AboutProject

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -4,22 +4,23 @@ import "./AboutProject.css"
 function AboutProject(){
   return(
     <section className="about-project">
-      <div className="about-project__info">
+
         <h2 className="about-project__title">
           О проекте
-          </h2>
+        </h2>
+        <div className="about-project__info">
         <h2 className="about-project__stage">
           Дипломный проект включал 5 этапов
-          </h2>
+        </h2>
         <p className="about-project__stage_info">
           Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.
-          </p>
+        </p>
         <h2 className="about-project__time">
           На выполнение диплома ушло 5 недель
-          </h2>
+        </h2>
         <h2 className="about-project__time_info">
           У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.
-          </h2>
+        </h2>
       </div>
       <div className="about-project__grid">
         <div className="about-project__grid_item">1 неделя</div>
