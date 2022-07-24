985cafa224ab6cda0912f5ecc0bfce65215b7cc3
GochKa <gocha.workout@gmail.com>
2022-07-25 00:54:17 +0300

    Адаптив планшета Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,7 @@ function Footer() {
   return (
     <section className="footer">
       <p className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className="footer__flex">
       <ul className="footer__list">
         <li className="footer__list_link">
           <a className="footer__list_link-item" href="https://practicum.yandex.ru/">  
@@ -23,6 +24,7 @@ function Footer() {
         </li>
       </ul>
       <p className="footer__copyright">&copy; 2022</p>
+      </div>
     </section>
   )
 }
