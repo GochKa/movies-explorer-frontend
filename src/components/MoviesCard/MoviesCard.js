609e2b7ea92562ab5f58dac769a6743716566c60
GochKa <gocha.workout@gmail.com>
2022-07-27 20:26:54 +0300

    Компонент MoviesCard

---
+++ src/components/MoviesCard/MoviesCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./MoviesCard.css"
+
+function MoviesCard(props){
+  return(
+    <section className="movies-card">
+      <div className="card">
+      <button className="button_save">Сохранить</button>
+      <img className="card__img" src={props.card} alt="" />
+        <div className="card__description">
+          <ul className="card__description-container">
+            <li className="card__title">{props.title}</li>
+            <li className="card__duration">{props.time}</li>
+          </ul>
+        </div>
+       </div>
+    </section>
+  )
+}
+
+export default MoviesCard;
