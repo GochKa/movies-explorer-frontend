fc3cfeac3fd9c5f2654f869352f41508813b2882
GochKa <gocha.workout@gmail.com>
2022-07-27 20:25:10 +0300

    Компонент Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom'
 import "./Header.css";
 import logo from "../../images/logo.svg";
 
 function Header(){
   return(
     <header className="header">
-      <img src={logo} className="header__logo" alt="logo"></img>
-      <button className="header__signup-button">Регистрация</button>
-      <button className="header__signin-button">Войти</button>
+      <img src={logo} className="header__logo" alt="logo" />
+      <Link to="signup" className="header__link">
+         Регистрация
+      </Link>
+      <Link to="signin" className="header__link">
+        Войти
+      </Link>
     </header>
   )
 }
