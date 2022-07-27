1b09a27b6e8c75b7c5d2a26198f2254b3f34ccbc
GochKa <gocha.workout@gmail.com>
2022-07-27 20:25:29 +0300

    Компонент More

---
+++ src/components/More/More.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../More/More.css";
+
+function More() {
+  return (
+    <section className="more">
+			<button type="button" className="more__button">Ещё</button>
+    </section>
+  );
+}
+
+export default More;
