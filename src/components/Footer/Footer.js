9292ad2cbe60a3fbd5beb3a59676bc40e71bd116
GochKa <gocha.workout@gmail.com>
2022-07-28 22:39:28 +0300

    Ревью: Компонент Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,29 +3,29 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <section className="footer">
+    <footer className="footer">
       <p className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</p>
       <div className="footer__flex">
       <ul className="footer__list">
         <li className="footer__list_link">
-          <a className="footer__list_link-item" href="https://practicum.yandex.ru/">  
+          <a target="blank" className="footer__list_link-item" href="https://practicum.yandex.ru/">  
             Яндекс.Практикум
           </a>
         </li>
         <li className="footer__list_link">
-          <a className="footer__list_link-item" href="https://github.com/GochKa"> 
+          <a target="blank" className="footer__list_link-item" href="https://github.com/GochKa"> 
           GitHub
           </a>
         </li>
         <li className="footer__list_link">
-          <a className="footer__list_link-item" href="https://vk.com/id211041864"> 
+          <a target="blank" className="footer__list_link-item" href="https://vk.com/id211041864"> 
           Вконтакте
           </a>
         </li>
       </ul>
       <p className="footer__copyright">&copy; 2022</p>
       </div>
-    </section>
+    </footer>
   )
 }
 
