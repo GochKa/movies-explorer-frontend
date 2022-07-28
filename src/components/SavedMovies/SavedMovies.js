8afd785d322f97f2e4217711b38b823551c02289
GochKa <gocha.workout@gmail.com>
2022-07-28 22:37:49 +0300

    Ревью: Компонент SavedMovies

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -7,10 +7,10 @@ import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import Footer from "../Footer/Footer";
 import More from "../More/More";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
-function SavedMovies(){
+function SavedMovies(props){
   return(
     <section className="savedmovies">
-      <NavTab />
+      <NavTab onClick={props.onMenu}/>
       <div className="movies__filter_desctop">
         <SearchForm />
         <FilterCheckbox />
