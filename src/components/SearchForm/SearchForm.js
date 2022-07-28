71cb764661b57c8b2eee65f9a418d26d0fd63d45
GochKa <gocha.workout@gmail.com>
2022-07-28 22:37:38 +0300

    Ревью: Компонент SearchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,7 +4,8 @@ import sbmbutton from "../../images/find_form.svg"
 function SearchForm(){
   return(
     <form className="searchform">
-      <input className="searchform__input" placeholder="Фильм"/>
+      <input className="searchform__input" placeholder="Фильм" required="true"/>
+      <span id="searchform-input-err" className="form-item-err"></span>
       <button type="submit" className="searchform__submit">
         <img src={sbmbutton} alt="кнопка формы"/>
       </button>
