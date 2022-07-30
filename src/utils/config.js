8e36756bb15d3028e4ac49163b69dead03bb2a39
GochKa <gocha.workout@gmail.com>
2022-07-30 18:29:44 +0300

    Директория utils
    Config, auth, MainApi(свой), MoviesApi(от Практикума)

---
+++ src/utils/config.js
@@ -0,0 +1,3 @@
+export const BASE_URL = "https://api.nomoreparties.co/beatfilm-movies";
+export const MAIN_API = "https://api.movies.gocha.nomoredomains.xyz/";
+export const AUTH = "https://api.movies.gocha.nomoredomains.xyz";
