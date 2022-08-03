f387ac7cc515677dbacab22e2159929623ef4664
GochKa <gocha.workout@gmail.com>
2022-08-04 01:09:15 +0300

    FilterChekbox

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -2,10 +2,14 @@ import React from "react";
 
 import './FilterCheckbox.css'
 
-function FilterCheckbox() {
+function FilterCheckbox(props) {
     return (
         <label className="filter-checkbox">
-            <input className="filter-checkbox__invisible-checkbox" type="checkbox" />
+            <input 
+                className="filter-checkbox__invisible-checkbox" 
+                type="checkbox"           
+                onChange={props.onFilter}
+                checked={props.isShortMovie}/>
             <div className="filter-checkbox__visible-checkbox">
                 <div className="filter-checkbox__toggle"></div>
             </div>
