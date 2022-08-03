0ccf5ab5cdf611ada1681a31e1cf8d09118eaba4
GochKa <gocha.workout@gmail.com>
2022-08-04 01:08:49 +0300

    All Movies components

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,16 +1,72 @@
 import React from "react";
 import "./MoviesCard.css"
+import mainApi from "../../utils/MainApi"
+function MoviesCard(props, {savedMoviesCardList}){
+  console.log(savedMoviesCardList)
+  const [isLiked, setIsLiked] = React.useState(false);
+  function onAddClick() {
+    if (!isLiked) {
+      mainApi
+        .addMovie({
+          movieId: props.id,
+          country: props.country,
+          director: props.director,
+          duration: props.duration,
+          year: props.year,
+          description: props.description,
+          image: `https://api.nomoreparties.co/${props.image.url}`,
+          trailerLink: props.trailerLink,
+          thumbnail: `https://api.nomoreparties.co/${props.image.formats.thumbnail.url}`,
+          nameRU: props.nameRU,
+          nameEN: props.nameEN,
+        })
+        .then(() => {
+          setIsLiked(true);
+        })
+        .catch((err) => console.log(err));
+    } else {
+      mainApi
+        .getUserMovies()
+        .then((savedmovies) => {
+          const newFilms = savedmovies.data.filter(
+            (item) => item.nameRU === props.nameRU
+          );
+          mainApi
+            .deleteMovie(newFilms[0]._id)
+            .then(() => {
+              console.log("1");
+            })
+            .catch((err) => console.log(err));
+        })
+        .catch((err) => console.log(`Ошибка загрузки данных: ${err}`));
+      setIsLiked(false);
+    }
+  }
 
-function MoviesCard(props){
+  
+
   return(
     <section className="movies-card">
       <div className="card">
-      <button className="button_save">Сохранить</button>
-      <img className="card__img" src={props.card} alt="Тут картинка" />
+      <button   className="button_save" onClick={onAddClick}>Сохранить</button>
+      <a         
+        href={props.trailerLink}
+        target="_blank"
+        rel="noopener noreferrer nofollow
+        ">
+      <img className="card__img"  alt={props.name}
+           src={`${
+            savedMoviesCardList
+              ? `${props.image}`
+              : `https://api.nomoreparties.co/${props.image.url}`
+          }`}/>
+        </a>
         <div className="card__description">
           <ul className="card__description-container">
-            <li className="card__title">{props.title}</li>
-            <li className="card__duration">{props.time}</li>
+            <li className="card__title">{props.name || props.movie.nameRU}</li>
+            <li className="card__duration">{`${Math.floor(
+            (props.duration || props.movie.duration) / 60
+          )}ч ${(props.duration || props.movie.duration) % 60}м`}</li>
           </ul>
         </div>
        </div>
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,17 +1,70 @@
 import React from "react";
 import "./MoviesCardList.css"
-
+import { MIN_NUMBER_OF_CARDS, MAX_NUMBER_OF_CARDS } from "../../utils/config";
 import MoviesCard from "../MoviesCard/MoviesCard";
-import card1 from "../../images/Card_1.svg"
-import card2 from "../../images/Card_2.svg"
-import card3 from "../../images/Card_3.svg"
-function MoviesCardList(){
+import More from "../More/More";
+
+function MoviesCardList(props, {savedMoviesCardList}){
+  const [isLikedMovie, setIsLikedMovie] = React.useState(false);
+  React.useEffect(() => {
+    const savedMovies = localStorage.getItem("savedMovies");
+    const films = JSON.parse(localStorage.getItem("films"));
+    if (savedMovies === null || films === null) {
+      return;
+    }
+    films.map((i) => {
+      const likedMovies = JSON.parse(savedMovies).includes(i.nameRU);
+      if (true) {
+        setIsLikedMovie(true);
+      }
+      console.log(likedMovies)
+      return likedMovies;
+    });
+    console.log(isLikedMovie)
+    
+  }, [isLikedMovie]);
+
+  const [counter, setCounter] = React.useState(3);
+
+  function showMoreMovies() {
+    setCounter(counter + 3);
+  }
+
   return(
+    <>
     <section className="movies-card-list">
-      <MoviesCard card={card1} title={"33 слова о дизайне"} time={"1ч 17м"}/>
-      <MoviesCard card={card2} title={"Киноальманах «100 лет дизайна»"} time={"1ч 17м"}/>
-      <MoviesCard card={card3} title={"В погоне за Бенкси"} time={"1ч 17м"}/>
+          {props.message ? (
+            <p className="movies-message">{props.message}</p>
+          ) : (
+            props.movies
+              .slice(0, counter)
+              .map((movie, id) => (
+                <MoviesCard
+                  movie={movie}
+                  name={movie.nameRU}
+                  duration={movie.duration}
+                  key={id}
+                  movieId={movie.movieId}
+                  {...movie}
+                  isSavedMovies={props.isSavedMovies}
+                  onAddMovie={props.onAddMovie}
+                  onDelete={props.onDelete}
+                  savedMovies={props.savedMovies}
+                  likedMovies={props.likedMovies}
+                  savedMoviesCardList={savedMoviesCardList}
+                />
+              ))
+          )}
     </section>
+    {props.movies.length >= MIN_NUMBER_OF_CARDS &&
+      props.movies.length > counter &&
+      props.movies.length <= MAX_NUMBER_OF_CARDS &&
+      !props.message ? (
+				<More showMoreMovies={showMoreMovies}  />
+      ) : (
+        ""
+      )}
+    </>
   )
 }
 
