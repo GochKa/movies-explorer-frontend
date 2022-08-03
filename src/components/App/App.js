f9ec6540fe925fa8880f7e4a25afb2a146ad8d70
GochKa <gocha.workout@gmail.com>
2022-08-04 01:09:26 +0300

    App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,82 +7,24 @@ import Movies from "../Movies/Movies";
 import Profile from "../Profile/Profile";
 import SavedMovies from "../SavedMovies/SavedMovies";
 import NotFound from "../NotFound/NotFound";
-import React, {useState, useEffect} from "react";
+import React from "react";
 import Navigation from "../Navigation/Navigation";
-import * as auth from "../../utils/auth"
-import  CurrentUserContext  from "../../context/CurrentUserContext"
-import mainApi from '../../utils/MainApi'
+import * as auth from "../../utils/Auth"
+import mainApi from "../../utils/MainApi"
+import * as moviesApi from "../../utils/MoviesApi"
+import CurrentUserContext from "../../contexts/CurrentUserContext"
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute"
+import {MAX_SHORT_MOVIE_DORATION} from "../../utils/config"
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [message, setMessage] = useState("");
-  const [currentUser, setCurrentUser] = React.useState({})
   const history = useHistory();
   let location = useLocation();
-
-
+  const [currentUser, setCurrentUser] = React.useState({});
+  const [message, setMessage] = React.useState("");
   
-//Эффект getUserInfo
-React.useEffect(() =>{
-  mainApi.getUserData()
-  .then((res) =>{
-    setCurrentUser(res)
-  })
-  .catch((err) =>{
-    console.error(err)
-  })
-}, []);
+///////////////////// Логин ///////////////////////////  
+//////////////////////////////////////////////////////
 
-function handleUpdateUser({ name, email }) {
-  mainApi
-    .editUserInfo({ name, email })
-    .then((userData) => {
-      setCurrentUser(userData.data);
-    })
-    .catch((err) => console.log(err));
-}
-
-  useEffect(() => {
-    const path = location.pathname;
-    const jwt = localStorage.getItem("jwt");
-    if (jwt !== null) {
-      auth
-        .getContent(jwt)
-        .then((res) => {
-          if (res) {
-            setLoggedIn(true);
-          
-            history.push(path);
-          }
-        })
-        .catch((err) => {
-          console.log(`Переданный токен некорректен или просрочек: ${err}`);
-          localStorage.removeItem("jwt");
-          history.push("/");
-        });
-    }
-    // }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  function handleRegister(name, email, password) {
-    auth
-      .register(name, email, password)
-      .then((res) => {
-        if (res) {
-          setMessage("");
-          handleLogin(email, password);
-          setLoggedIn(true);
-        }
-      })
-      .catch((err) => {
-        if (err === 409) {
-          setMessage("Пользователь с таким email уже существует");
-        } else {
-          setMessage("При регистрации пользователя произошла ошибка");
-        }
-      });
-  }
-
+  const [loggedIn, setLoggedIn] = React.useState(false);
   function handleLogin(email, password) {
     auth
       .authorize(email, password)
@@ -95,6 +37,7 @@ function handleUpdateUser({ name, email }) {
           localStorage.setItem("jwt", data.token);
           setMessage("");
           setLoggedIn(true);
+          getCurrentUser();
           history.push("/movies");
           return loggedIn;
         }
@@ -110,6 +53,207 @@ function handleUpdateUser({ name, email }) {
         localStorage.removeItem("jwt");
       });
   }
+//////////////////////////////////////////////////////
+
+
+///////////////////// Регистрация ///////////////////////////  
+////////////////////////////////////////////////////////////
+
+function handleRegister(name, email, password) {
+  auth
+    .register(name, email, password)
+    .then((res) => {
+      if (res) {
+        setMessage("");
+        handleLogin(email, password);
+        setLoggedIn(true);
+        setCurrentUser(res);
+      }
+    })
+    .catch((err) => {
+      if (err === 409) {
+        setMessage("Пользователь с таким email уже существует");
+      } else {
+        setMessage("При регистрации пользователя произошла ошибка");
+      }
+    });
+}
+////////////////////////////////////////////////////////////
+
+
+///////////////////// getCurrentUser ///////////////////////////  
+///////////////////////////////////////////////////////////////
+
+  function getCurrentUser() {
+    const jwt = localStorage.getItem("jwt");
+    mainApi
+      .getUserData(jwt)
+      .then((userData) => {
+        if (userData) {
+          setCurrentUser(userData);
+          localStorage.setItem("currentUser", JSON.stringify(userData));
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        localStorage.removeItem("jwt");
+        localStorage.removeItem("currentUser");
+      });
+  }
+///////////////////////////////////////////////////////////////
+
+React.useEffect(() => {
+  const path = location.pathname;
+  const jwt = localStorage.getItem("jwt");
+  if (jwt !== null) {
+    auth
+      .getContent(jwt)
+      .then((res) => {
+        if (res) {
+          setLoggedIn(true);
+          getCurrentUser();
+          setCurrentUser(res);
+          history.push(path);
+        }
+      })
+      .catch((err) => {
+        console.log(`Переданный токен некорректен или просрочек: ${err}`);
+        localStorage.removeItem("jwt");
+        history.push("/");
+      });
+  }
+  // }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+}, []);
+  
+///////////////////// Получение массива фильмов ///////////////////////////  
+//////////////////////////////////////////////////////////////////////////  
+const [movies, setMovies] = React.useState([]);
+React.useEffect(() => {
+  moviesApi
+    .getInitialMovies()
+    .then((allMovies) => {
+      setMovies(allMovies);
+      localStorage.setItem("movies", JSON.stringify(allMovies));
+    })
+    .catch((err) => {
+      console.log(`Ошибка: ${err}`);
+      localStorage.removeItem("movies");
+    });
+}, [currentUser]);
+//////////////////////////////////////////////////////////////////////////  
+  
+///////////////////// Фильтрация фильмов ///////////////////////////  
+///////////////////////////////////////////////////////////////////
+const [shortMovies, setShortMovies] = React.useState(false);
+function filterShortMovies(arr) {
+  if (arr.length !== 0 || arr !== "undefind") {
+    return arr.filter((movie) =>
+      shortMovies ? movie.duration <= MAX_SHORT_MOVIE_DORATION : true
+    );
+  }
+}
+
+///////////////////// Поиск фильма ///////////////////////////  
+
+const [moviesMessage, setMoviesMessage] = React.useState("");
+const [userMovies, setUserMovies] = React.useState([]);
+const [sortedMovies, setSortedMovies] = React.useState([]);
+
+function handleGetMovies(keyword) {
+  setMoviesMessage("");
+  const key = new RegExp(keyword, "gi");
+  const findedMovies = movies.filter(
+    (item) => key.test(item.nameRU) || key.test(item.nameEN)
+  );
+  if (findedMovies.length === 0) {
+    setMoviesMessage("Ничего не найдено");
+  } else {
+    setMoviesMessage("");
+    const checkedLikes = findedMovies.map((movie) => {
+      movie.isSaved = userMovies.some(
+        (userMovie) => userMovie.movieId === movie.id
+        
+      );
+
+      return movie;
+    });
+    setSortedMovies(checkedLikes);
+    localStorage.setItem("sortedMovies", JSON.stringify(checkedLikes));
+  }
+}
+//////////////////////////////////////////////////////////////
+  
+
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+function handleCheckBox() {
+  setShortMovies(!shortMovies);
+}
+///////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////
+React.useEffect(() => {
+  const jwt = localStorage.getItem("jwt");
+  if (jwt !== null) {
+    Promise.all([mainApi.getUserData(jwt), mainApi.getUserMovies(jwt)])
+      .then(([userData, savedMovies]) => {
+
+        localStorage.setItem("currentUser", JSON.stringify(userData));
+        setCurrentUser(userData);
+
+        const savedMoviesList = savedMovies.filter(
+          (item) => item.owner._id === userData._id
+        );
+        localStorage.setItem("userMovies", JSON.stringify(savedMoviesList));
+        setUserMovies(savedMoviesList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}, [loggedIn]);
+///////////////////////////////////////////////////////////////////
+
+/////////////////// Изменение профиля ///////////////////
+function handleUpdateUser(data) {
+  mainApi
+    .editUserInfo(data)
+    .then((editedData) => {
+      setCurrentUser(editedData);
+      setMessage("Данные профиля успешно обновлены");
+    })
+    .catch((err) => {
+      console.log(`Ошибка: ${err}`);
+      if (err.status === 409) {
+        setMessage("Пользователь с таким email уже существует");
+      } else {
+        setMessage("При изменении данных профиля произошла ошибка");
+      }
+    });
+}
+
+/////////////////// Изменение состояния лайка ///////////////////
+
+///////////////////////////////////////////////////////////////////
+
+
+function handleGetSavedMovies(keyword) {
+  setMoviesMessage("");
+  const key = new RegExp(keyword, "gi");
+  const findedMovies = userMovies.filter(
+    (item) => key.test(item.nameRU) || key.test(item.nameEN)
+  );
+  if (findedMovies.length === 0) {
+    setMoviesMessage("Ничего не найдено");
+  } else {
+    setMoviesMessage("");
+    setUserMovies(findedMovies);
+  }
+}
 
   const [isNavigationOpen , setNavigationOpen] = React.useState(false)
 
@@ -126,23 +270,50 @@ function handleUpdateUser({ name, email }) {
       <Route exact path='/'>
         <Main />
       </Route>
-      <Route exact path='/movies'>
-        <Movies onMenu={navigationClick}/>
-      </Route>
+      <ProtectedRoute 
+        path='/movies'
+        component={Movies} 
+        onMenu={navigationClick}
+        loggedIn={loggedIn}
+        movies={filterShortMovies(sortedMovies)}
+        onGetMovies={handleGetMovies}
+        message={moviesMessage}
+        onFilter={handleCheckBox}
+        isShortMovie={shortMovies}
+        savedMovies={userMovies}
+      />
       <Route exact path='/signup'>
-        <Register onRegister={handleRegister} message={message}/>
+        <Register 
+        onRegister={handleRegister} 
+        message={message}/>
       </Route>
       <Route exact path="/signin">
-        <Login onLogin={handleLogin} loggedIn={loggedIn} message={message}/></Route>
-      <Route exact path="/profile">
-        <Profile onMenu={navigationClick} handleUpdateUser={handleUpdateUser} loggedIn={loggedIn}/>
+        <Login 
+        onLogin={handleLogin} 
+        loggedIn={loggedIn} 
+        message={message}/>
       </Route>
-      <Route exact path="/saved-movies">
-        <SavedMovies onMenu={navigationClick}/>
-      </Route>
-      <Route exact path="*">
-        <NotFound />
-      </Route>
+      <ProtectedRoute 
+        path="/profile" 
+        onMenu={navigationClick}
+        component={Profile}
+        loggedIn={loggedIn}
+        message={message}
+        onEditUser={handleUpdateUser}
+      />
+      <ProtectedRoute 
+        path="/saved-movies"
+        component={SavedMovies}
+        onMenu={navigationClick} 
+        movies={filterShortMovies(userMovies)}
+        onGetMovies={handleGetSavedMovies}
+        loggedIn={loggedIn}
+        isShortMovie={shortMovies}
+        onFilter={handleCheckBox}
+        message={moviesMessage}
+        isSavedMovies={true}
+      />
+      <Route exact path="*"><NotFound /></Route>
     </Switch>
     <Navigation isOpen={isNavigationOpen} onClose={closeNavigation}/>
     </CurrentUserContext.Provider>
