bd6b7ebd8cedb551bc145d9bcf04603d21ef66eb
GochKa <gocha.workout@gmail.com>
2022-08-04 01:07:31 +0300

    seacrhForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,48 @@
 import React from "react";
 import "./SearchForm.css";
 import sbmbutton from "../../images/find_form.svg"
-function SearchForm(){
+function SearchForm(props){
+  const [findedMovie, setFindedMovie] = React.useState("");
+  const [error, setError] = React.useState("");
+  const [formValid, setFormValid] = React.useState(false);
+
+  function handleSearchMovie(e) {
+    setFindedMovie(e.target.value);
+    if (e.target.value.length === 0) {
+      setError("Нужно ввести ключевое слово");
+    } else {
+      setError("");
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setError("");
+    props.onGetMovies(findedMovie);
+    setFindedMovie("");
+  }
+
+  React.useEffect(() => {
+    if (findedMovie && !error) {
+      setFormValid(true);
+    } else {
+      setFormValid(false);
+    }
+  }, [findedMovie, error]);
+
   return(
-    <form className="searchform">
-      <input className="searchform__input" placeholder="Фильм" required/>
+    <form className="searchform" onSubmit={handleSubmit}>
+      <input className="searchform__input" 
+            placeholder="Фильм" 
+            required={true}
+            minLength="2"
+            maxLength="40"
+            value={findedMovie}
+            onChange={handleSearchMovie}/>
       <span id="searchform-input-err" className="form-item-err"></span>
-      <button type="submit" className="searchform__submit">
+      <button type="submit" className="searchform__submit" 
+          onClick={handleSubmit}
+          disabled={!formValid}>
         <img src={sbmbutton} alt="кнопка формы"/>
       </button>
     </form>
