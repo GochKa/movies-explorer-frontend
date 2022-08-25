928bea508e9848d02307579f10630c0d5a7027a7
GochKa <gocha.workout@gmail.com>
2022-08-25 13:04:36 +0300

    Update MoviesCard.js

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,19 +1,41 @@
 import React from "react";
 import "./MoviesCard.css"
 
+import { baseUrl } from "../../utils/config";
 function MoviesCard(props){
+
+
   return(
     <section className="movies-card">
-      <div className="card">
-      <button className="button_save">Сохранить</button>
-      <img className="card__img" src={props.card} alt="Тут картинка" />
+          <button
+            className="button_save"
+            > Сохранить
+          </button>
+      <a  href={props.trailerLink}
+        target="_blank"
+        rel="noopener noreferrer nofollow">
+      <img
+          className="card__img"
+          alt={props.name}
+          src={
+            props.isSavedMovies
+              ? props.movie.image
+              : `${baseUrl}${
+                  props.movie.image ? props.movie.image.url : props.image
+                }`
+          } />
+          </a>
         <div className="card__description">
           <ul className="card__description-container">
-            <li className="card__title">{props.title}</li>
-            <li className="card__duration">{props.time}</li>
+            <li className="card__title">{props.name || props.movie.nameRU}</li>
+            <li className="card__duration">
+                {`${Math.floor(
+                 (props.duration || props.movie.duration) / 60
+                 )}ч ${(props.duration || props.movie.duration) % 60}м`
+                }
+          </li>
           </ul>
         </div>
-       </div>
     </section>
   )
 }
