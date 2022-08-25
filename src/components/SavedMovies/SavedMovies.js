23dd9276929d44cce9eba38e87b08aab435b2413
GochKa <gocha.workout@gmail.com>
2022-08-25 13:04:23 +0300

    Update SavedMovies

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -5,18 +5,29 @@ import NavTab from "../NavTab/NavTab";
 import SearchForm from "../SearchForm/SearchForm";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import Footer from "../Footer/Footer";
-import More from "../More/More";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 function SavedMovies(props){
+
   return(
     <section className="savedmovies">
       <NavTab onClick={props.onMenu}/>
       <div className="movies__filter_desctop">
-        <SearchForm />
-        <FilterCheckbox />
+        <SearchForm onGetMovies={props.onGetMovies}/>
+        <FilterCheckbox         
+          onFilter={props.onFilter}
+          isShortMovie={props.isShortMovie}/>
       </div>
-      <MoviesCardList />
-      <More />
+      {props.movies.length > 0 ? (
+        <MoviesCardList
+          isSavedMovies={props.isSavedMovies}
+					movies={props.movies}
+          onGetMovies={props.onGetMovies}
+          onDelete={props.onDelete}
+          message={props.message}
+        />
+      ) : (
+        <p className="movies-message">У вас пока нет сохраненных фильмов</p>
+      )}
       <Footer />
     </section>
   )
